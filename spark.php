2593d4bc3877113baab2f1f7a86d11534c76e597
gulinavarro <gulinavarrolink@gmail.com>
2015-12-06 15:23:37 -0300

    Create spark.php

---
+++ spark.php
@@ -0,0 +1,12 @@
+<?php
+// mysqli is required to work automatically.
+class Transcendence
+{
+    public function __construct()
+    {
+        
+    }
+    
+}
+
+$Transcendence = new Transcendence;
